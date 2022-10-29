13212dd4eab24dcf6ccfd72ec31168bd12790552
Rooban <ruben_t@hotmail.de>
2022-10-29 21:01:17 +0200

    Create player fields
    For each 11 players a card is created

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,303 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    home: MyApp(),
+  ));
 }
 
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Recommended Lineup"),
+      ),
+      body: ListView(
+        reverse: true,
+        children: [
+          Column(
+            children: [
+              Row(
+                children: <Widget>[
+                  Spacer(),
+                  SizedBox(
+                    width: 100,
+                    child: Card(
+                        color: Colors.white54,
+                        child: Column(
+                          children: [
+                            const FlutterLogo(),
+                            ListTile(
+                              title: const Center(
+                                child: Text("title"),
+                              ),
+                              subtitle: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: const [
+                                  Text("Subt1"),
+                                  Text("Subt2"),
+                                ],
+                              ),
+                            ),
+                          ],
+                        )),
+                  ),
+                  SizedBox(
+                    width: 100,
+                    child: Card(
+                        color: Colors.white54,
+                        child: Column(
+                          children: [
+                            const FlutterLogo(),
+                            ListTile(
+                              title: const Center(
+                                child: Text("title"),
+                              ),
+                              subtitle: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: const [
+                                  Text("Subt1"),
+                                  Text("Subt2"),
+                                ],
+                              ),
+                            ),
+                          ],
+                        )),
+                  ),
+                  SizedBox(
+                    width: 100,
+                    child: Card(
+                        color: Colors.white54,
+                        child: Column(
+                          children: [
+                            const FlutterLogo(),
+                            ListTile(
+                              title: const Center(
+                                child: Text("title"),
+                              ),
+                              subtitle: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: const [
+                                  Text("Subt1"),
+                                  Text("Subt2"),
+                                ],
+                              ),
+                            ),
+                          ],
+                        )),
+                  ),
+                  Spacer(),
+                ],
+              ),
+              //Row 2:
+              Row(
+                children: <Widget>[
+                  Spacer(),
+                  SizedBox(
+                    width: 100,
+                    child: Card(
+                        color: Colors.white54,
+                        child: Column(
+                          children: [
+                            const FlutterLogo(),
+                            ListTile(
+                              title: const Center(
+                                child: Text("title"),
+                              ),
+                              subtitle: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: const [
+                                  Text("Subt1"),
+                                  Text("Subt2"),
+                                ],
+                              ),
+                            ),
+                          ],
+                        )),
+                  ),
+                  SizedBox(
+                    width: 100,
+                    child: Card(
+                        color: Colors.white54,
+                        child: Column(
+                          children: [
+                            const FlutterLogo(),
+                            ListTile(
+                              title: const Center(
+                                child: Text("title"),
+                              ),
+                              subtitle: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: const [
+                                  Text("Subt1"),
+                                  Text("Subt2"),
+                                ],
+                              ),
+                            ),
+                          ],
+                        )),
+                  ),
+                  SizedBox(
+                    width: 100,
+                    child: Card(
+                        color: Colors.white54,
+                        child: Column(
+                          children: [
+                            const FlutterLogo(),
+                            ListTile(
+                              title: const Center(
+                                child: Text("title"),
+                              ),
+                              subtitle: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: const [
+                                  Text("Subt1"),
+                                  Text("Subt2"),
+                                ],
+                              ),
+                            ),
+                          ],
+                        )),
+                  ),
+                  SizedBox(
+                    width: 100,
+                    child: Card(
+                        color: Colors.white54,
+                        child: Column(
+                          children: [
+                            const FlutterLogo(),
+                            ListTile(
+                              title: const Center(
+                                child: Text("title"),
+                              ),
+                              subtitle: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: const [
+                                  Text("Subt1"),
+                                  Text("Subt2"),
+                                ],
+                              ),
+                            ),
+                          ],
+                        )),
+                  ),
+                  Spacer(),
+                ],
+              ),
+              //Row 3:
+              Row(
+                children: <Widget>[
+                  Spacer(),
+                  SizedBox(
+                    width: 100,
+                    child: Card(
+                        color: Colors.white54,
+                        child: Column(
+                          children: [
+                            const FlutterLogo(),
+                            ListTile(
+                              title: const Center(
+                                child: Text("title"),
+                              ),
+                              subtitle: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: const [
+                                  Text("Subt1"),
+                                  Text("Subt2"),
+                                ],
+                              ),
+                            ),
+                          ],
+                        )),
+                  ),
+                  SizedBox(
+                    width: 100,
+                    child: Card(
+                        color: Colors.white54,
+                        child: Column(
+                          children: [
+                            const FlutterLogo(),
+                            ListTile(
+                              title: const Center(
+                                child: Text("title"),
+                              ),
+                              subtitle: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: const [
+                                  Text("Subt1"),
+                                  Text("Subt2"),
+                                ],
+                              ),
+                            ),
+                          ],
+                        )),
+                  ),
+                  SizedBox(
+                    width: 100,
+                    child: Card(
+                        color: Colors.white54,
+                        child: Column(
+                          children: [
+                            const FlutterLogo(),
+                            ListTile(
+                              title: const Center(
+                                child: Text("title"),
+                              ),
+                              subtitle: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: const [
+                                  Text("Subt1"),
+                                  Text("Subt2"),
+                                ],
+                              ),
+                            ),
+                          ],
+                        )),
+                  ),
+                  Spacer(),
+                ],
+              ),
+              //Row4-Goalkeeper:
+              Row(
+                children: <Widget>[
+                  Spacer(),
+                  SizedBox(
+                    width: 100,
+                    child: Card(
+                        color: Colors.white54,
+                        child: Column(
+                          children: [
+                            const FlutterLogo(),
+                            ListTile(
+                              title: const Center(
+                                child: Text("title"),
+                              ),
+                              subtitle: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: const [
+                                  Text("Subt1"),
+                                  Text("Subt2"),
+                                ],
+                              ),
+                            ),
+                          ],
+                        )),
+                  ),
+                  Spacer(),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
